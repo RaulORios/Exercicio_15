a125ca15ebce19d336b2f0de0b459df41ed08066
RaulORios <raul.orios89@outlook.com>
2021-08-04 18:37:38 -0300

    Program.cs adicionado

---
+++ src/Exercicio_15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*********     Exercicio 15     *********");
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine("");
+
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            if (numero >= 0)
+            {
+                Console.WriteLine($"O número {numero} é POSITIVO.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} é NEGATIVO.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
